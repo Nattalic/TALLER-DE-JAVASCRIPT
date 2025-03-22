15b252172a9d2fa66013e179f4b81b7a68559c63
Nattalic <nordonez376@gmail.com>
2025-03-22 17:48:26 -0500

    se agregaron colores

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 document.getElementById("calcular").addEventListener("click", function() {
     // OBTENER VARIABLES 
-    let op1 = document.getElementById("operador1").value;
-    let operador = document.getElementById("operadores").value;
-    let op2 = document.getElementById("operador2").value;
+    let operador1 = document.getElementById("operador1").value;
+    let operadores = document.getElementById("operadores").value;
+    let operador2 = document.getElementById("operador2").value;
     let resultado = document.getElementById("resultado");
-    let num1 = Number(op1);
-    let num2 = Number(op2);
+    let num1 = parseInt(operador1);
+    let num2 = parseInt(operador2);
     
     // VALIDAR SI ES UN NUMERO
-    if (parseInt(op1) || parseInt(op2)) {
-        resultado.textContent = "ERROR: Ingresa números válidos.";
+    if (isNaN(operador1) || isNaN(operador2)) {
+        resultado.textContent = "ERROR: Ingresa números válidos.  ❌";
         return;
     }
 
-    // Validar operador y realizar operación
-    if (operador === "+") {
-        resultado.textContent = "Resultado: " + (num1 + num2);
-    } else if (operador === "-") {
-        resultado.textContent = "Resultado: " + (num1 - num2);
-    } else if (operador === "**") {
-        resultado.textContent = "Resultado: " + (num1 ** num2);
-    } else if (operador === "%") {
-        resultado.textContent = "Resultado: " + (num1 % num2);
-    } else if (operador === "*") {
-        resultado.textContent = "Resultado: " + (num1 * num2);
-    } else if (operador === "/") {
+    // REALIZAR OPERACIÓN 
+    if (operadores === "+") {
+        resultado.textContent = "Resultado: " + (num1 + num2) + " :)";
+    } else if (operadores === "-") {
+        resultado.textContent = "Resultado: " + (num1 - num2) + " :)";
+    } else if (operadores === "**") {
+        resultado.textContent = "Resultado: " + (num1 ** num2) + " :)";
+    } else if (operadores === "%") {
+        resultado.textContent = "Resultado: " + (num1 % num2) + " :)";
+    } else if (operadores === "*") {
+        resultado.textContent = "Resultado: " + (num1 * num2) + " :)";
+    } else if (operadores === "/") {
         if (num2 === 0) {
-            resultado.textContent = "ERROR: ¡No se puede dividir por cero!";
+            resultado.textContent = "ERROR: ¡No se puede dividir por cero!  ❌"; 
         } else {
-            resultado.textContent = "Resultado: " + (num1 / num2);
+            resultado.textContent = "Resultado: " + (num1 / num2) + " :)";
         }
     } else {
-        resultado.textContent = "ERROR: Operador No Válido!";
+        resultado.textContent = "ERROR: Operador No Válido!  ❌"; //VALIDAR OPERADOR 
     }
 });
+    // FIN
